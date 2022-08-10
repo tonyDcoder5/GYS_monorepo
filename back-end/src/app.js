273319e547b8d77e4fb1d727dcc5fd8b7d54cdbf
Nahia Riviera <n.riviera.dev@gmail.com>
2022-08-10 12:12:20 -0500

    generated base files, implemented routes

---
+++ back-end/src/app.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
+const express = require("express");
+const cors = require("cors");
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+//import routers TO DO****************
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+app.use("/events", eventsRouter);
+app.use("/resources", resourcesRouter);
+app.use("/downloads", downloadsRouter);
+app.use("/media", mediaRouter);
+app.use("/contacts", contactsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
