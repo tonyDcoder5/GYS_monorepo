fadd44baf083b0c2c8efbc9144190592e33928dd
TonyDCoder5 <tonyaflex5@gmail.com>
2022-08-20 20:14:53 -0600

    finalized navbar styling & responsiveness

--- front-end/src/Resources/LocalSupport.js
+++ front-end/src/Resources/LocalSupport.js
@@ -1,45 +1,72 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Card, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-
 export default function LocalSupport() {
-    const contacts = [
-        {
-          id: 0,
-          orgName: "TestOrg1",
-          orgDesc: "Example description",
-          orgPhone: "123-456-7890",
-          orgUrl: "#",
-        },
-        {
-          id: 1,
-          orgName: "TestOrg1",
-          orgDesc: "Example description",
-          orgPhone: "123-456-7890",
-          orgUrl: "#",
-        },
-        {
-          id: 2,
-          orgName: "TestOrg1",
-          orgDesc: "Example description",
-          orgPhone: "123-456-7890",
-          orgUrl: "#",
-        },
-        {
-          id: 3,
-          orgName: "Suicide & Crisis Lifeline",
-          orgDesc:
-            "988 has been designated as the new three-digit dialing code that will route callers to the National Suicide Prevention Lifeline.",
-          orgPhone: "988",
-          orgUrl: "https://988lifeline.org/",
-        },
-      ];
+  const contacts = [
+    {
+      id: 0,
+      orgName: "TestOrg1",
+      orgDesc: "Example description",
+      orgPhone: "123-456-7890",
+      orgUrl: "#",
+      contact: "Org Contact",
+      contact_email: "sameple@sample.com",
+      icon: require("../assets/img/GYS_logo.jpg"),
+    },
+    {
+      id: 1,
+      orgName: "TestOrg2",
+      orgDesc: "Example description",
+      orgPhone: "123-456-7890",
+      orgUrl: "#",
+      contact: "Org Contact",
+      contact_email: "sameple@sample.com",
+      icon: require("../assets/img/GYS_logo.jpg"),
+    },
+    {
+      id: 2,
+      orgName: "TestOrg3",
+      orgDesc: "Example description",
+      orgPhone: "123-456-7890",
+      orgUrl: "#",
+      contact: "Org Contact",
+      contact_email: "sameple@sample.com",
+      icon: require("../assets/img/GYS_logo.jpg"),
+    },
+    {
+      id: 3,
+      orgName: "Suicide & Crisis Lifeline",
+      orgDesc:
+        "988 has been designated as the new three-digit dialing code that will route callers to the National Suicide Prevention Lifeline.",
+      orgPhone: "988",
+      orgUrl: "https://988lifeline.org/",
+      contact: "Org Contact",
+      contact_email: "sameple@sample.com",
+      icon: require("../assets/img/GYS_logo.jpg"),
+    },
+  ];
 
-    return(
-        <>
-        <h3>Local Support Section</h3>
-        <div className="resource-block">
+  const initialCard = {
+    id: 0,
+    orgName: "Click an Icon",
+    orgDesc: "To see more details on our local partnered organizations, or click below to find out how to become one!",
+    orgUrl: "#",
+    contact: "Org Contact",
+    contact_email: "sameple@sample.com",
+    icon: require("../assets/img/GYS_logo.jpg"),
+  }
+  const [hiCard, setHiCard] = useState({ initialCard });
+
+  const clickHandle = (contact) => {
+    console.log(contact);
+    setHiCard(contact);
+  }
+
+  return (
+    <>
+      <h3>Local Support Section</h3>
+      <div className="resource-block">
         <h6>Block 2- Contact Section</h6>
         <div className="row">
           <div className="col">
@@ -47,28 +74,26 @@ export default function LocalSupport() {
               <p>Section Title</p>
             </div>
             <Row xs={1} md={2} className="g-4">
-                {contacts.map((contact, idx)=> (
-                    <Col>
-                    <button>Test {idx}</button>
-                    </Col>
-                ))}
+              {contacts.map((contact, idx) => (
+                <Col key={idx}>
+                  <img onClick={()=> clickHandle(contact)} key={contact.id} src={contact.icon} width="25%" />
+                </Col>
+              ))}
             </Row>
           </div>
           <div className="col">
-            <Card style={{ width: "auto", height: "auto"}}>
-              <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: "100%", height: "100%", color: "#fff" }} className="bg-dark">
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{hiCard.orgName}</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  {hiCard.orgDesc}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary"><a href={hiCard.orgUrl}>Website</a></Button>
               </Card.Body>
             </Card>
           </div>
         </div>
       </div>
-      </>
-    )
+    </>
+  );
 }
--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -2,44 +2,65 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../assets/img/GYS_logo.jpg";
-import {listResources} from ".././utils/api"
+import { listResources } from ".././utils/api";
 
 export default function NavBar() {
-  const [resources,setResources] = useState(null);
-  const [resourcesError,setResourcesError] = useState(null)
-  useEffect( ()=>{
-    const fetchResources = async ()=>{
-      try{
-        const abortController = new AbortController();
-        const response = await listResources(abortController.signal);
-        setResources(response)
-        
-      } catch (error){
-        setResourcesError(error)
+  const [resources, setResources] = useState(null);
+  const [resourcesError, setResourcesError] = useState(null);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
       }
     }
-    fetchResources()
-  },[])
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
   
+  useEffect(() => {
+    const fetchResources = async () => {
+      try {
+        const abortController = new AbortController();
+        const response = await listResources(abortController.signal);
+        setResources(response);
+      } catch (error) {
+        setResourcesError(error);
+      }
+    };
+    fetchResources();
+  }, []);
+
   return (
-    <div className="header-nav">
-      <div className=" col-9 mt-2">
-        <Navbar sticky="top">
-          <img src={logo} alt="GYS logo" />
-          <Navbar.Brand href="home">Golf Yankee Six</Navbar.Brand>
-          <Nav className="nav-links">
-            <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
-            <Nav.Link href="resources">Resources</Nav.Link>
-            <Nav.Link href="support">Blog</Nav.Link>
-          </Nav>
+    <div className="header-nav row">
+        <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+          <div>
+            <img src={logo} alt="GYS logo" />
+            <Navbar.Brand href="home">Golf Yankee Six</Navbar.Brand>
+          </div>
+          <div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav"> 
+            <Nav className="nav-links">
+              <Nav.Link href="home">Home</Nav.Link>
+              <Nav.Link href="about">About</Nav.Link>
+              <Nav.Link href="resources">Resources</Nav.Link>
+              <Nav.Link href="support">Blog</Nav.Link>
+            </Nav>
+              <button className="btn support-btn">
+                <Link to="support">Support</Link>
+              </button>
+              <button className="btn events-btn">
+                <Link to="resources">Events</Link>
+              </button>
+          </Navbar.Collapse>
+          </div>
         </Navbar>
       </div>
-      <div className="col-3 nav-buttons">
-        <button className="btn support-btn">
-          <Link to="support">Support</Link></button>
-        <button className="btn events-btn"><Link to="resources">Events</Link></button>
-      </div>
-    </div>
   );
 }
